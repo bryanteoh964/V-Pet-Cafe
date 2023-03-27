7bcc549d6f2472d63f2956883f3058e4d24d0b51
bryanteoh964 <68241796+bryanteoh964@users.noreply.github.com>
2023-03-27 15:44:48 -0400

    Cat cafe start page and playground page basic set up

---
+++ cafe/src/components/PlayGround.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data() {
+    return 0
+  }
+}
+</script>
+
+<template>
+    <img src="..\assets\cat-9161.png">
+</template>
+
+<style>
+    
+</style>
---
+++ cafe/src/components/StartPage.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+    data() {
+      return 0
+    }
+  }
+</script>
+
+<template>
+  <div class="start_page">
+    <h1>Welcome to the V-Pet Cafe!</h1><br>
+    <a href="#/main">Login</a> 
+  </div>
+</template>
+
+<style>
+  .start_page {
+    margin: auto;
+    width: 50%;
+    border: 3px solid green;
+    padding: 10px;
+  }
+</style>
+
