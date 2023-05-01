ec0351b40f911cf41a8ffc8e3414267cfd58f34f
paulitab <plopezburgos@gmail.com>
2023-05-01 17:17:00 -0400

    updated database to implement more stats including the image, added new routes

--- kitchen/routes/catstat.js
+++ kitchen/routes/catstat.js
@@ -31,4 +31,26 @@ router.delete("/delete", async (req, res) => {
     }
 });
 
+router.post("/upCat/:catId&:catImg", async (req, res) => {
+    const cat = req.params.catId.split("=")[1]
+    const catImg = req.params.catImg.split("=")[1]
+    console.log(cat)
+    try{
+        await Cat.findOneAndUpdate({name: cat}, {image: catImg})
+        res.send("Updated")
+    }
+    catch (err) {
+        res.send(err)
+    }
+})
+
+router.get("/getCat/:owner", async (req, res) => {
+    const owner = req.params.owner
+    try {
+        const cat = await Cat.findOne({user: owner})
+        res.send(cat)
+    }  catch (err) {
+        res.send(err)
+    }
+})
 module.exports = router;
